23069f45cc0dc5c442892e2e29d02a391d4fd933
silentorb <christopherwjohnson@gmail.com>
2015-02-12 12:00:36 -0700

    Fixed a rare bug involving updates with composite identities involving references.

--- lib/schema/Property.ts
+++ lib/schema/Property.ts
@@ -199,7 +199,7 @@ module Ground {
       if (property_type && property_type.parent)
         return this.get_sql_value(value, property_type.parent.name, is_reference);
 
-      if (this.parent.primary_key == this.name) {
+      if (this.parent.primary_key == this.name && this.type != 'reference') {
         value = this.parent.get_identity2(value)
       }
 
